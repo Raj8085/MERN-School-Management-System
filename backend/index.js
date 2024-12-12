3763e1b03918737fc375d08c8da33fbf14e0ceee
Rajeev-patel <patelrajeev10342@gmail.com>
2024-12-13 01:23:57 +0530

    cors added

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,41 @@ const PORT = process.env.PORT || 5000
 
 dotenv.config();
 
+const allowedOrigins = [
+    'http://localhost:3000',
+    // Add more origins as needed
+  ];
+  
+  // Define CORS options
+  const corsOptions = {
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  };
+
 // app.use(bodyParser.json({ limit: '10mb', extended: true }))
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(cors(corsOptions))
 
+
+
+
+
+
+
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
